ffdf3da7773c0b21e92e07a299009e757ec71342
Munjal Dhamecha <mastermunj@gmail.com>
2024-03-13 19:33:37 +0530

    chore: update dependencies

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -4,8 +4,10 @@ const typeEnum = configConventional.rules['type-enum'];
 typeEnum[2].push('init');
 
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    '@commitlint/config-conventional'
+  ],
   rules: {
     'type-enum': typeEnum,
-  },
+  }
 };
